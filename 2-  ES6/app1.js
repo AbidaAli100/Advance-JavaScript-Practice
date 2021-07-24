bb902fa76ffd6cd61475e04031a96121416c0d52
AbidaAli100 <abida.ali100@gmail.com>
2021-07-24 10:53:48 +0500

    practiced const in ES6

---
+++ 2-  ES6/app1.js
@@ -0,0 +1,14 @@
+// Practice Problems
+
+// Create a constant and try assigning it another value and see what happens.
+
+const Age = 22;
+const Age = 23;
+console.log(Age);
+
+// Identifier 'Age' has already been declared.
+
+/*
+This is the error we get
+becayse we cannot ressign a value to const, 
+*/
