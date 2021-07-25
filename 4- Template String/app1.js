eca62cdd5b6045e1f4f768980ae0299a6bdd47de
AbidaAli100 <abida.ali100@gmail.com>
2021-07-25 11:28:44 +0500

    string tamplate practiced

---
+++ 4- Template String/app1.js
@@ -0,0 +1,21 @@
+// window.onload = function() {
+//     var temp = `My name is Abida Ali`
+//     console.log(temp);
+// }
+
+//second example
+
+window.onload = function() {
+    function studentData(name, Age) {
+        console.log("The student Name is " + name + " and She is " + Age + "years old.");
+        console.log(`Students name is ${name} and she is ${Age} years Old`); //easy way
+
+    }
+    studentData("Sara", 22)
+}
+
+
+//third prob
+
+let carName = "Corolla";
+console.log(`this is a ${carName} car.`)
