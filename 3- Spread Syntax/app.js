509ea7b3adc7af2bc90b37a05a23643c65f604cf
AbidaAli100 <abida.ali100@gmail.com>
2021-07-28 20:09:50 +0500

    practiced ES6 basics

---
+++ 3- Spread Syntax/app.js
@@ -0,0 +1,64 @@
+// function myName() {
+//     var name = "Abida";
+
+// }
+// myName();
+// console.log("waqas", name);
+
+
+
+
+// var age = 40;
+
+// function getAge() {
+//     console.log(age);
+// }
+// console.log(window);
+// getAge();
+
+// function getAge() {
+//     var age = 24;
+//     console.log(age)
+// }
+// getAge()
+
+
+// let a = 24;
+// let b = 22;
+// console.log(a > b);
+// if (true) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+
+
+
+function myName() {
+    if (true) {
+
+        var name1 = "waQas";
+        let name = "wakas";
+        const name2 = "Abida Aly";
+    }
+    console.log("second name", name1);
+    console.log("first name", name);
+    console.log("third name", name2);
+}
+myName();
+
+// function fruits() {
+//     if (true) {
+//         var f2 = "apple";
+//         let f1 = "banana";
+//         const f3 = "strawberry";
+//     }
+
+//     function fruits1() {
+//         console.log(f2);
+//         console.log(f1);
+//         console.log(f3);
+//     }
+//     fruits1()
+// }
+// fruits()
